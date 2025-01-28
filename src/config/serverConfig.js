6a673436d2085173f8ed004223666a39a42671d6
sid <siddhuwagh27112004@gmail.com>
2025-01-28 09:15:26 +0530

    setting up body-parser

--- src/config/serverConfig.js
+++ src/config/serverConfig.js
@@ -1,5 +1,6 @@
-const dotenv = require('dotenv');
 
+const dotenv = require('dotenv');
+//using enviornment variables to maintain their secrecy Ex:for api key we cant just show it to all
 dotenv.config();
 module.exports = {
     PORT:process.env.PORT
